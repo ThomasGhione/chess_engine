4b7a720df392dad022143cba3983befcb469f934
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-11-20 18:25:59 +0100

    better menu, fixed turns, added rook moving (not tested), general optimization

--- legalmoves.cpp
+++ legalmoves.cpp
@@ -1,3 +1,44 @@
 #include "chessengine.h"
 
-// check if move is legit 
+namespace chess {
+
+    // check if move is legit
+    bool IsMoveValid(char player, unsigned char piece, board chessboard[ML][ML], int rank1, int file1, int rank2, int file2) {
+
+        // check if piece is white or black
+        if (player == 'W') piece -= WHITE;  // piece is white
+        else piece -= BLACK;                // piece is black
+
+
+        // check which piece i'm using and get the legal moves
+        switch (piece) {
+
+            case (PAWN):
+                // if pawn is white it goes '^' otherwise it goes 'v'
+                if (player == 'W') {
+                    
+                } else {
+
+                }
+
+            case (KNIGHT):
+
+            case (BISHOP):
+
+            case (ROOK):
+                if (rank2 == rank1) return true;
+                if (file2 == file1) return true;
+                return false;
+                
+                
+            case (QUEEN):
+
+            case (KING):
+
+
+            default: throw std::invalid_argument("Invalid piece");
+        }
+    }
+
+
+}
--- turn.cpp
+++ turn.cpp
@@ -7,23 +7,39 @@ namespace chess {
 
     }
 
+    int fromCharToInt(char file) {
+        file = std::tolower(file);
+        switch (file) {
+            case 'a': return 0;
+            case 'b': return 1;
+            case 'c': return 2;
+            case 'd': return 3;
+            case 'e': return 4;
+            case 'f': return 5;
+            case 'g': return 6;
+            case 'h': return 7;
+            default: throw std::invalid_argument("invalid file coord");
+        }
+    }
 
 
     //void input move
     void inputMove(board chessboard[ML][ML], char &player) {
-        std::cout << "input your move: ";
-        int iRank1, iFile1, iRank2, iFile2;
-        std::cin >> iRank1 >> iFile1 >> iRank2 >> iFile2;
+        std::cout << "\nInput your move\nFormat: FILE1 RANK1 FILE2 RANK2\nOr press Q to quit\nInput: ";
+        int iRank1, iRank2;
+        char iFile1, iFile2;
+        if (iFile1 == 'Q') throw std::invalid_argument("QUIT"); //TODO make better quit
+        std::cin >> iFile1 >> iRank1 >> iFile2 >> iRank2;
 
         //check if input is valid (u can't choose your opponent pieces)
         switch (player) {
             case 'W':
-                if (chessboard[iFile1][iRank1].piece < WHITE)
+                if (chessboard[iRank1 - 1][fromCharToInt(iFile1)].piece > (unsigned char)(WHITE | KING))
                     throw std::invalid_argument("u chose ur opponent piece");
                 player = 'B';
                 break;
             case 'B':
-                if (chessboard[iFile1][iRank1].piece >= WHITE)
+                if (chessboard[iRank1 - 1][fromCharToInt(iFile1)].piece < (unsigned char)BLACK)
                     throw std::invalid_argument("u chose ur opponent piece");
                 player = 'W';
                 break;
@@ -31,25 +47,25 @@ namespace chess {
         }
         
         //move the piece to the selected square
-        chessboard[iFile2][iRank2].piece = chessboard[iFile1][iRank1].piece;        
+        chessboard[iRank2 - 1][fromCharToInt(iFile2)].piece = chessboard[iRank1 - 1][fromCharToInt(iFile1)].piece;        
 
         //delete the previous square piece
-        chessboard[iFile1][iRank1].piece = EMPTY;
+        chessboard[iRank1 - 1][fromCharToInt(iFile1)].piece = EMPTY;
 
         debugprint(chessboard);
     }
 
     //start of the game
-    char gameStarts(board chessboard[ML][ML]) {
+    char gameStarts() {
         std::cout << "MENU:\n    W: play as white\n    B: play as black\n    S: save (to add)\n    L: load last game (to add)\n    Q: quit\nINPUT: ";
         char player;
         std::cin >> player;
         switch (player) {
             case 'W': return 'W';
-            case 'B': return 'B';
-            case 'S': return 'S'; //TODO IMPLEMENT SAVE
-            case 'L': return 'L'; //TODO IMPLEMENT LOAD
-            case 'Q': throw std::logic_error("quit");
+            case 'B': return 'B';                         //TODO WILL BE FINISHED AFTER CREATING (AT LEAST) THE ENGINE PROTOTYPE
+            case 'S': return 'S';                         //TODO IMPLEMENT SAVE
+            case 'L': return 'L';                         //TODO IMPLEMENT LOAD
+            case 'Q': throw std::logic_error("quit");     //TODO MAKE BETTER QUIT
             default: throw std::invalid_argument("player must be either white (W) or black (B)");
         }
     }
