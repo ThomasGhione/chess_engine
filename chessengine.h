fed23d35192ea22a472490149a9f033fce199138
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-11-28 19:43:56 +0100

    better legalmoves.cpp but not finished to implement it yet

--- chessengine.h
+++ chessengine.h
@@ -22,7 +22,9 @@
 //     the first 2 bits define which player (01 meaning white, 10 meaning black)
 //     the last 6 bits define which piece we're referring to
 // DEFINE ID FOR PIECES:
+
 #define EMPTY 0x00      // 00000000
+
 #define PAWN 0x01       // 00000001    
 #define KNIGHT 0x02     // 00000010
 #define BISHOP 0x04     // 00000100
@@ -30,7 +32,10 @@
 #define QUEEN 0x10      // 00010000
 #define KING 0x20       // 00100000
 #define WHITE 0x40      // 01000000
-#define BLACK 0x80      // 10000000 
+#define BLACK 0x80      // 10000000
+
+#define PIECEMASK 0x3F  // 00111111 
+#define PLAYERMASK 0xC0 // 11000000
 
 namespace chess {
     const char ML = 8; // ML = MAX_LINE
--- legalmoves.cpp
+++ legalmoves.cpp
@@ -4,30 +4,35 @@ namespace chess {
 
     // check if move is legit
     bool isMoveValid(char player, board chessboard[ML][ML], int rank1, int file1, int rank2, int file2) {
-
+        std::cout << (chessboard[rank1][file1].piece == (WHITE | PAWN)) << std::endl; // 01 000001
         // get the piece ID by subtracting the 1st 2bits
-        (player == 'W') ? chessboard[rank1][file1].piece -= WHITE : chessboard[rank1][file1].piece -= BLACK;      
-
+        //(player == 'W') ? chessboard[rank1][file1].piece -= WHITE : chessboard[rank1][file1].piece -= BLACK;      
+        chessboard[rank1][file1].piece &= PIECEMASK; 
+        std::cout << (chessboard[rank1][file1].piece == PAWN) << std::endl;
         // check which piece i'm using and get the legal moves
         switch (chessboard[rank1][file1].piece) {
 
             case (PAWN):
+                if (file1 != file2) return false; // pawn can't move diagonally, if so then return false
+
                 // if pawn is white it goes '^' otherwise it goes 'v'
                 if (player == 'W') {
-                    if (file1 != file2) return false; // pawn can't move diagonally, if so then return false
-
-                    if (rank1 == 1) // if pawn is on starting position then it can move either 1 or 2 squares forward
-                        return (rank2 == 2 || rank2 == 3); 
-                    if (rank1 > 1) // else it can move only 1 square
-                        if (rank2 == 7) return true; //TODO promote pawn 
+                    if (rank1 == 2) // if pawn is on starting position then it can move either 1 or 2 squares forward
+                        return (rank2 == 3 || rank2 == 4); 
+                    if (rank1 > 2) { // else it can move only 1 square
+                        if (rank2 == 8) return true; //TODO promote pawn 
                         return (rank2 == rank1 + 1);
-                    return false; // TODO throw exception
+                    } return false; // TODO throw exception
                 }
-                else {
-                    if (rank2 < rank1) return true;
-                    if (file2 != file1) return false;
+                if (player == 'B') {
+                    if (rank1 == 7)
+                        return (rank2 == 6 || rank2 == 5);
+                    if (rank1 < 7) {
+                        if (rank2 == 1) return true; //TODO promote pawn
+                        return (rank2 == rank1 - 1);
+                    } return false; // TODO throw exception
                 }
-                return true; //! PLACEHOLDER
+                throw std::invalid_argument("pawn isn't white nor black");
             case (KNIGHT):
                 return true; //! PLACEHOLDER
             case (BISHOP):
@@ -40,7 +45,8 @@ namespace chess {
                 return true; //! PLACEHOLDER
             case (KING):
                 return true; //! PLACEHOLDER
-            default: throw std::invalid_argument("Invalid piece");
+            //default: throw std::invalid_argument("Invalid piece");
+            default: return true; //TODO REMOVE THIS PLACEHOLDER
         }
     }
     
