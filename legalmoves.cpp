bead329886b7e23ecfc66653113808d74a34c637
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-12-05 14:47:45 +0100

    added bishop and queen move

--- legalmoves.cpp
+++ legalmoves.cpp
@@ -4,7 +4,12 @@ namespace chess {
 
     // check if move is legit
     bool isMoveValid(char player, board chessboard[ML][ML], int rank1, int file1, int rank2, int file2) {
-        // get the piece ID by getting the 1st 2bits = 00   
+        // get the piece ID by getting the 1st 2bits = 00  
+
+        // you can't take your own pieces
+        if (((chessboard[rank2 - 1][file2 - 1].piece & PLAYERMASK) == WHITE) && (player == 'W')) return false;
+        if (((chessboard[rank2 - 1][file2 - 1].piece & PLAYERMASK) == BLACK) && (player == 'B')) return false;
+
         switch (chessboard[rank1 - 1][file1 - 1].piece & PIECEMASK) { //! check which piece i'm using and get the legal moves
             
             case (PAWN):
@@ -34,13 +39,34 @@ namespace chess {
                         ((rank2 == rank1 - 2) && (file2 == file1 - 1 || file2 == file1 + 1)) ||
                         ((rank2 == rank1 - 1) && (file2 == file1 - 2 || file2 == file1 + 2)));
             case (BISHOP):
-                return true; //! PLACEHOLDER
+                int trank, tfile;
+                for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) 
+                    if ((rank2 == trank + 1) && (file2 == tfile + 1)) return true;
+                for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) 
+                    if ((rank2 == trank + 1) && (file2 == tfile - 1)) return true;
+                for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) 
+                    if ((rank2 == trank - 1) && (file2 == tfile + 1)) return true;
+                for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) 
+                    if ((rank2 == trank - 1) && (file2 == tfile - 1)) return true;
+                return false;
             case (ROOK):
                 return ((rank2 == rank1) != (file2 == file1)); // if rank (or file) is the same then move is valid (!= means XOR)
             case (QUEEN):
-                return true; //! PLACEHOLDER
-            case (KING):
-                return true; //! PLACEHOLDER
+                if ((rank2 == rank1) != (file2 == file1)) return true;
+                //int trank, tfile;
+                for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) 
+                    if ((rank2 == trank + 1) && (file2 == tfile + 1)) return true;
+                for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) 
+                    if ((rank2 == trank + 1) && (file2 == tfile - 1)) return true;
+                for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) 
+                    if ((rank2 == trank - 1) && (file2 == tfile + 1)) return true;
+                for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) 
+                    if ((rank2 == trank - 1) && (file2 == tfile - 1)) return true;
+                return false; //! PLACEHOLDER
+            case (KING): //TODO implement check/checkmate/castle
+                if ((rank2 == rank1 + 1) || (rank2 == rank1 - 1) || (rank2 == rank1))
+                    return ((file2 == file1 + 1) || (file2 == file1 - 1) || (file2 == file1));
+                return false;  
             //default: throw std::invalid_argument("Invalid piece");
             default: return true; //TODO REMOVE THIS PLACEHOLDER
         }
