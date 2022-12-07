18c5c8e9621129ae1443f920b9a0f2d4aea54c6e
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-12-07 15:11:12 +0100

    fixed pawn moves & moptimizations in legalmoves.cpp

--- legalmoves.cpp
+++ legalmoves.cpp
@@ -2,7 +2,7 @@
 
 namespace chess {
 
-    bool promotePawn(char player, board chessboard[ML][ML], int rank1, int file1) {
+    bool promotePawn(unsigned char player, board chessboard[ML][ML], int rank1, int file1) {
         char promotedPawn;
         std::cout << "What do you want to promote your pown to?\nChoose between 'N', 'B', 'R' or 'Q': ";
         wrongOption:
@@ -11,28 +11,18 @@ namespace chess {
             std::cout << "Option isn't valid! Choose again: ";
             goto wrongOption;
         }
-        if (player == 'W')
-            switch (promotedPawn) {
-                case 'N': chessboard[rank1 - 1][file1 - 1].piece = WHITE | KNIGHT; return true;
-                case 'B': chessboard[rank1 - 1][file1 - 1].piece = WHITE | BISHOP; return true;
-                case 'R': chessboard[rank1 - 1][file1 - 1].piece = WHITE | ROOK; return true;
-                case 'Q': chessboard[rank1 - 1][file1 - 1].piece = WHITE | QUEEN; return true;
-                default: throw std::logic_error("unexpected behaviour");
-            }
-        if (player == 'B')
-            switch (promotedPawn) {
-                case 'N': chessboard[rank1 - 1][file1 - 1].piece = BLACK | KNIGHT; return true;
-                case 'B': chessboard[rank1 - 1][file1 - 1].piece = BLACK | BISHOP; return true;
-                case 'R': chessboard[rank1 - 1][file1 - 1].piece = BLACK | ROOK; return true;
-                case 'Q': chessboard[rank1 - 1][file1 - 1].piece = BLACK | QUEEN; return true;
-                default: throw std::logic_error("unexpected behaviour");
-            }
-        throw std::logic_error("unexpected behaviour");
+        
+        switch (promotedPawn) {
+            case 'N': chessboard[rank1 - 1][file1 - 1].piece = player | KNIGHT; return true;
+            case 'B': chessboard[rank1 - 1][file1 - 1].piece = player | BISHOP; return true;
+            case 'R': chessboard[rank1 - 1][file1 - 1].piece = player | ROOK; return true;
+            case 'Q': chessboard[rank1 - 1][file1 - 1].piece = player | QUEEN; return true;
+            default: throw std::invalid_argument("invalid argument");
+        }
     }
 
     // check if move is legit
-    bool isMoveValid(char player, board chessboard[ML][ML], int rank1, int file1, int rank2, int file2) {
-        // get the piece ID by getting the 1st 2bits = 00  
+    bool isMoveValid(unsigned char player, board chessboard[ML][ML], int rank1, int file1, int rank2, int file2) {
 
         // you can't take your own pieces
         if (((chessboard[rank2 - 1][file2 - 1].piece & PLAYERMASK) == WHITE) && (player == 'W')) return false;
@@ -41,10 +31,12 @@ namespace chess {
         switch (chessboard[rank1 - 1][file1 - 1].piece & PIECEMASK) { //! check which piece i'm using and get the legal moves
             
             case (PAWN):
-                if (file1 != file2) return false; // pawn can't move diagonally, if so then return false
+                //TODO en passant
+                if (file1 != file2) // check if file2 is different to file1 (pawns can't move diagonally but they can take pieces on the square next to the diagonal)
+                    if (!((file1 == file2 + 1) || (file1 == file2 - 1)) || chessboard[rank2 - 1][file2 - 1].piece == EMPTY) return false; 
 
-                // if pawn is white it goes '^' otherwise it goes 'v'
-                if (player == 'W') {
+                if (player == WHITE) {
+
                     if (rank1 == 2) // if pawn is on starting position then it can move either 1 or 2 squares forward
                         return (rank2 == 3 || rank2 == 4); 
                     if (rank1 > 2) { // else it can move only 1 square
@@ -53,7 +45,7 @@ namespace chess {
                         return promotePawn(player, chessboard, rank1, file1);
                     } throw std::invalid_argument("invalid pawn");
                 }
-                if (player == 'B') {
+                if (player == BLACK) {
                     if (rank1 == 7)
                         return (rank2 == 6 || rank2 == 5);
                     if (rank1 < 7) {
@@ -62,7 +54,7 @@ namespace chess {
                         return promotePawn(player, chessboard, rank1, file1);
                     } throw std::invalid_argument("invalid pawn");
                 }
-                throw std::invalid_argument("pawn isn't white nor black");
+                throw std::invalid_argument("pawn is neither white or black");
             case (KNIGHT):
                 return (((rank2 == rank1 + 2) && (file2 == file1 - 1 || file2 == file1 + 1)) ||
                         ((rank2 == rank1 + 1) && (file2 == file1 - 2 || file2 == file1 + 2)) ||
@@ -72,11 +64,11 @@ namespace chess {
                 int trank, tfile;
                 for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) 
                     if ((rank2 == trank + 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) 
+                for (trank = rank1, tfile = file1; trank < 9 || tfile; ++trank, --tfile) 
                     if ((rank2 == trank + 1) && (file2 == tfile - 1)) return true;
-                for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) 
+                for (trank = rank1, tfile = file1; trank || tfile < 9; --trank, ++tfile) 
                     if ((rank2 == trank - 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) 
+                for (trank = rank1, tfile = file1; trank || tfile; --trank, --tfile) 
                     if ((rank2 == trank - 1) && (file2 == tfile - 1)) return true;
                 return false;
             case (ROOK):
@@ -85,19 +77,18 @@ namespace chess {
                 if ((rank2 == rank1) != (file2 == file1)) return true;
                 for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) 
                     if ((rank2 == trank + 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) 
+                for (trank = rank1, tfile = file1; trank < 9 || tfile; ++trank, --tfile) 
                     if ((rank2 == trank + 1) && (file2 == tfile - 1)) return true;
-                for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) 
+                for (trank = rank1, tfile = file1; trank || tfile < 9; --trank, ++tfile) 
                     if ((rank2 == trank - 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) 
+                for (trank = rank1, tfile = file1; trank || tfile; --trank, --tfile) 
                     if ((rank2 == trank - 1) && (file2 == tfile - 1)) return true;
                 return false;
             case (KING): //TODO implement check/checkmate/castle
                 if ((rank2 == rank1 + 1) || (rank2 == rank1 - 1) || (rank2 == rank1))
                     return ((file2 == file1 + 1) || (file2 == file1 - 1) || (file2 == file1));
                 return false;  
-            //default: throw std::invalid_argument("Invalid piece");
-            default: return true; //TODO REMOVE THIS PLACEHOLDER
+            default: throw std::invalid_argument("Invalid piece");
         }
     }
     
