e209a4d19bcbba52abced380c94610029ef58e76
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-12-11 01:56:44 +0100

    added bishop 'collisions'

--- legalmoves.cpp
+++ legalmoves.cpp
@@ -39,8 +39,7 @@ namespace chess {
 
                 if (player == WHITE) {
 
-                    if (rank1 == 2) // if pawn is on starting position then it can move either 1 or 2 squares forward
-                        return (rank2 == 3 || rank2 == 4); 
+                    if (rank1 == 2) return (rank2 == 3 || rank2 == 4); // if pawn is on starting position then it can move only 1 or 2 squares forward
                     if (rank1 > 2) { // else it can move only 1 square
                         if (rank2 != rank1 + 1) return false; // can't move more than 1 square at time  
                         if (rank2 != 8) return true; // if rank2 != 8 then return true, otherwise promote the pawn
@@ -48,8 +47,7 @@ namespace chess {
                     } throw std::invalid_argument("invalid pawn");
                 }
                 if (player == BLACK) {
-                    if (rank1 == 7)
-                        return (rank2 == 6 || rank2 == 5);
+                    if (rank1 == 7) return (rank2 == 6 || rank2 == 5);
                     if (rank1 < 7) {
                         if (rank2 != rank1 - 1) return false;
                         if (rank2 != 1) return true;
@@ -64,15 +62,36 @@ namespace chess {
                         ((rank2 == rank1 - 1) && (file2 == file1 - 2 || file2 == file1 + 2)));
             case (BISHOP):
                 int trank, tfile;
-                for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) 
-                    if ((rank2 == trank + 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank < 9 || tfile; ++trank, --tfile) 
-                    if ((rank2 == trank + 1) && (file2 == tfile - 1)) return true;
-                for (trank = rank1, tfile = file1; trank || tfile < 9; --trank, ++tfile) 
-                    if ((rank2 == trank - 1) && (file2 == tfile + 1)) return true;
-                for (trank = rank1, tfile = file1; trank || tfile; --trank, --tfile) 
-                    if ((rank2 == trank - 1) && (file2 == tfile - 1)) return true;
-                return false;
+                bool flag;
+                if ((rank2 > rank1) && (file2 > file1)) {
+                    flag = false;
+                    for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) if ((rank2 == trank + 1) && (file2 == tfile + 1)) flag = true;
+                    if (flag) {
+                        for (trank = rank1 + 1 , tfile = file1 + 1; trank < rank2 || tfile < file2; ++trank, ++tfile) if (cb[trank - 1][tfile - 1].piece) return false;
+                        return true;
+                    }  return false;
+                } if ((rank2 > rank1) && (file2 < file1)) {
+                    flag = false;
+                    for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) if ((rank2 == trank + 1) && (file2 == tfile - 1)) flag = true;
+                    if (flag) {
+                        for (trank = rank1 + 1 , tfile = file1 - 1; trank < rank2 || tfile > file2; ++trank, --tfile) if (cb[trank - 1][tfile - 1].piece) return false;
+                        return true;
+                    } return false;
+                } if ((rank2 < rank1) && (file2 > file1)) {
+                    flag = false;
+                    for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) if ((rank2 == trank - 1) && (file2 == tfile + 1)) flag = true;
+                    if (flag) {
+                        for (trank = rank1 - 1 , tfile = file1 + 1; trank > rank2 || tfile < file2; --trank, ++tfile) if (cb[trank - 1][tfile - 1].piece) return false;
+                        return true;
+                    } return false;
+                } if ((rank2 < rank1) && (file2 < file1)) {
+                    flag = false;
+                    for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) if ((rank2 == trank - 1) && (file2 == tfile - 1)) flag = true; 
+                    if (flag) {
+                        for (trank = rank1 - 1 , tfile = file1 - 1; trank > rank2 || tfile > file2; --trank, --tfile) if (cb[trank - 1][tfile - 1].piece) return false;
+                        return true;
+                    } return false;
+                } return false; // if we arrived here it means none of the previous options were valied, therefore the outcome must be false
             case (ROOK):
                 if ((rank2 == rank1) != (file2 == file1)) { // rooks can move like a "+", "!="" = "XOR" (both equations can't be true at the same time)
                     if (rank1 < rank2) for (int trank = rank1 + 1; trank < rank2; ++trank) if (cb[trank - 1][file2 - 1].piece) return false; // "cb[trank - 1][file2 - 1].piece" = "cb[trank - 1][file2 - 1].piece != EMPTY"
