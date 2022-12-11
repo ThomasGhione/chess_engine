12854436147feadaf807156e4c54cd6866d58ff8
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-12-11 19:48:59 +0100

    fixed pawns and optimized

--- legalmoves.cpp
+++ legalmoves.cpp
@@ -2,7 +2,7 @@
 
 namespace chess {
 
-    bool promotePawn(unsigned char player, board cb[ML][ML], int rank1, int file1) {
+    bool promotePawn(board cb[ML][ML], unsigned char &player, int &rank1, int &file1) {
         char promotedPawn;
         std::cout << "What do you want to promote your pown to?\nChoose between 'N', 'B', 'R' or 'Q': ";
         wrongOption:
@@ -33,28 +33,25 @@ namespace chess {
 
     bool bishopMove(board cb[ML][ML], int &rank1, int &file1, int &rank2, int &file2) {
         int trank, tfile;
-        bool flag = false;;
+        bool flag = false;
+        if ((rank2 == rank1) || (file2 == file1)) return false; // if we arrive here it means the coords given don't correspond to how the bishop moves
         if ((rank2 > rank1) && (file2 > file1)) {
             for (trank = rank1, tfile = file1; trank < 9 || tfile < 9; ++trank, ++tfile) if ((rank2 == trank + 1) && (file2 == tfile + 1)) flag = true;
             if (!flag) return false;
             for (trank = rank1 + 1, tfile = file1 + 1; trank < rank2 || tfile < file2; ++trank, ++tfile) if (cb[trank - 1][tfile - 1].piece) return false;
-            return true;
         } if ((rank2 > rank1) && (file2 < file1)) {
             for (trank = rank1, tfile = file1; trank < 9 || tfile > 0; ++trank, --tfile) if ((rank2 == trank + 1) && (file2 == tfile - 1)) flag = true;
             if (!flag) return false;
             for (trank = rank1 + 1, tfile = file1 - 1; trank < rank2 || tfile > file2; ++trank, --tfile) if (cb[trank - 1][tfile - 1].piece) return false;
-            return true;
         } if ((rank2 < rank1) && (file2 > file1)) {
             for (trank = rank1, tfile = file1; trank > 0 || tfile < 9; --trank, ++tfile) if ((rank2 == trank - 1) && (file2 == tfile + 1)) flag = true;
             if (!flag) return false;
             for (trank = rank1 - 1, tfile = file1 + 1; trank > rank2 || tfile < file2; --trank, ++tfile) if (cb[trank - 1][tfile - 1].piece) return false;
-            return true;
         } if ((rank2 < rank1) && (file2 < file1)) {
             for (trank = rank1, tfile = file1; trank > 0 || tfile > 0; --trank, --tfile) if ((rank2 == trank - 1) && (file2 == tfile - 1)) flag = true; 
             if (!flag) return false;
             for (trank = rank1 - 1, tfile = file1 - 1; trank > rank2 || tfile > file2; --trank, --tfile) if (cb[trank - 1][tfile - 1].piece) return false;
-            return true;
-        } return false; // if we arrived here it means none of the previous options were valied, therefore the outcome must be false
+        } return true;
     }
 
     // check if move is legit
@@ -68,26 +65,22 @@ namespace chess {
             
             case (PAWN):
                 //TODO en passant
-                if (file1 != file2) // check if file2 is different to file1 (pawns can't move diagonally but they can take pieces on the square next to the diagonal)
-                    if (!((file1 == file2 + 1) || (file1 == file2 - 1)) || cb[rank2 - 1][file2 - 1].piece == EMPTY) return false; 
+                if (file1 != file2) { // check if file2 is different to file1 (pawns can't move diagonally but they can take pieces on the square next to the diagonal)
+                    if (!((file2 == file1 + 1) || (file2 == file1 - 1)) || cb[rank2 - 1][file2 - 1].piece == EMPTY) return false;
+                } else if (cb[rank2 - 1][file2 - 1].piece != EMPTY) return false; // can't move in a square already occupied if the pawn isn't taking any pieces
 
-                if (cb[rank2 - 1][file2 - 1].piece != EMPTY) return false; // can't move in a square already occupied
-
-                if (player == WHITE) {
-                    if (rank1 == 2) return (rank2 == 3 || rank2 == 4); // if pawn is on starting position then it can move only 1 or 2 squares forward
-                    if (rank1 > 2) { // else it can move only 1 square
-                        if (rank2 != rank1 + 1) return false; // can't move more than 1 square at time  
-                        if (rank2 != 8) return true; // if rank2 != 8 then return true, otherwise promote the pawn
-                        return promotePawn(player, cb, rank1, file1);
-                    }
-                } if (player == BLACK) {
-                    if (rank1 == 7) return (rank2 == 6 || rank2 == 5);
-                    if (rank1 < 7) {
-                        if (rank2 != rank1 - 1) return false;
-                        if (rank2 != 1) return true;
-                        return promotePawn(player, cb, rank1, file1);
-                    }
-                } throw std::invalid_argument("pawn is neither white or black");
+                switch (player) {
+                    case (WHITE):
+                        if (rank1 == 2) return (rank2 == 3 || rank2 == 4);     // if pawn is on starting position then it can move only 1 or 2 squares forward
+                        if (rank2 != rank1 + 1) return false;                  // else it can only move 1 square forward
+                        if (rank2 != 8) return true;                           // if rank2 != 8 then return true, otherwise promote the pawn
+                        break;                                                 // break so it jumps to "return promotePawn(...)"
+                    case (BLACK):                                              // same goes for BLACK pawns:
+                        if (rank1 == 7) return (rank2 == 6 || rank2 == 5);     // starting square (1 or 2 squares)
+                        if (rank2 != rank1 - 1) return false;                  // not starting square (1 square)
+                        if (rank2 != 1) return true;                           // if not on promotion square simply return true
+                        break;                                                 // otherwise brak to promote
+                } return promotePawn(cb, player, rank1, file1);                // if we arrive here it means the only option left was the promotion
             case (KNIGHT): return (((rank2 == rank1 + 2) && (file2 == file1 - 1 || file2 == file1 + 1)) ||
                                    ((rank2 == rank1 + 1) && (file2 == file1 - 2 || file2 == file1 + 2)) ||
                                    ((rank2 == rank1 - 2) && (file2 == file1 - 1 || file2 == file1 + 1)) ||
