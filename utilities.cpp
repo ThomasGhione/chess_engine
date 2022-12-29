dd2752ca1642862f1b6f92233dddbba2f106105d
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2022-12-29 17:05:24 +0100

    small changes

---
+++ utilities.cpp
@@ -0,0 +1,13 @@
+#include "chessengine.h"
+
+namespace chess {
+
+    std::string playerString(const unsigned char &player) noexcept {
+        return (player == WHITE) ? "WHITE" : "BLACK";
+    }
+
+    int fromCharToInt(const char file) noexcept {
+        return std::tolower(file) - 96;
+    }
+
+}
