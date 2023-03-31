64af6eefbd27c65902c7c8cb65f223061fc78d45
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2023-03-31 08:03:39 +0100

    test2

---
+++ cs/Pieces/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using chess_engine_cs.ColorType;
+using chess_engine_cs.Pieces.types;
+
+namespace chess_engine_cs.Pieces
+{
+    class Piece
+    {
+        public Color Color { get; set; }
+        public char Symbol { get; protected set; }
+        public PieceTypes PieceType { get; protected set; }
+
+        public Piece(Color color)
+        {
+            Color = color;
+        }
+    }
+}
