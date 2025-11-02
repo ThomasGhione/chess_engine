b639d9576ff4a3a1994764b3773b2ab59e76cd42
Simo64Terminator <simo64.tomasella@gmail.com>
2025-11-02 17:43:07 +0100

    Deleting chess.exe file in run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 chcp 65001
+rm -f chess.exe
 g++ -std=c++23 -Wall -Wextra -Wpedantic -Wno-unused-parameter -DDEBUG -O2 main.cpp ./coords/*.cpp ./engine/*.cpp ./piece/*.cpp ./printer/*.cpp ./driver/*.cpp -o "chess"
 chess.exe
