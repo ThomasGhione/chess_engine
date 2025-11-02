31f7728994ec219f3a59b615135d8def1f0809bb
Simo64Terminator <simo64.tomasella@gmail.com>
2025-11-02 17:45:02 +0100

    upsie

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 chcp 65001
-rm -f chess.exe
+rm chess.exe
 g++ -std=c++23 -Wall -Wextra -Wpedantic -Wno-unused-parameter -DDEBUG -O2 main.cpp ./coords/*.cpp ./engine/*.cpp ./piece/*.cpp ./printer/*.cpp ./driver/*.cpp -o "chess"
 chess.exe
