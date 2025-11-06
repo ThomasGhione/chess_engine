b412169db338213e5c548e9b4ad9c49ad6b5afd0
Daniele <daniele.ferretti.uni@gmail.com>
2025-11-06 15:07:20 +0100

    Add 2 tests about knigth

--- tests/testPiece.cpp
+++ tests/testPiece.cpp
@@ -86,9 +86,31 @@ ut::suite piceSuite = [] {
     // 1nbqkbn1/1pppppp1/r7/p4R1p/P3r2P/R7/1PPPPPP1/1NBQKBN1 w - - 8 7
 
     // Controllare che possa raggiungere queste caselle:
-    // a1 a2 b3 c3 d3 e3 f3 g3 h3 
+    // a1 a2 b3 c3 d3 e3 f3 g3 h3
     // a7 a8 b6 c6 d6 e6 f6 g6 h6
     // a4 b4 c4 d4 f4 g4 h4
     // a5 b5 c5 d5 e5 g5 h5
+  };
+
+  "Knight"_test = []{
+    // In posizione con fen:
+    // r3k2r/ppqn1p1p/2p1pnpP/2PpN3/3P4/2NQP3/PP3PP1/R3K2R w KQkq - 2 15
+
+    // Controllare che cavalli possano raggiungere:
+    // Da c3: b1 d1 e2 e4 d5 b5 a4
+    // Da e5: f3 g4 g6 f7 d7 c6 c4
+    // Da d7: b8 b6 c5 e5 f8
+    // Da f6: e4 g4 h5 g8
+  };
+  
+  "Knight"_test = []{
+    // In posizione con fen:
+    // r3k2r/pp1nbpp1/1qp1p2p/3p1b2/2PPnB2/1QN2NP1/PP2PPBP/R3R1K1 w kq - 2 11
+
+    // Controllare che cavalli possano raggiungere:
+    // Da c3: b1 d1 e4 d5 b5 a4
+    // Da f3: d2 e5 g5 h4
+    // Da e4: c3 d2 f2 g3 g5 f6 d6 c5
+    // Da d7: b8 c5 e5 f6 f8
   };
 };
