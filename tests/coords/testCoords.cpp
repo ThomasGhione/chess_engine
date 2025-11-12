3bb79c9ceb17397f9d8f35332056bcdef1bbfb69
Daniele <daniele.ferretti.uni@gmail.com>
2025-11-12 16:19:44 +0100

    Rework to tests structure

--- tests/coords/testCoords.cpp
+++ tests/coords/testCoords.cpp
@@ -10,55 +10,6 @@ namespace ut = boost::ut;
 ut::suite coordsSuite = [] {
   using namespace ut;
 
-  "Invalid parameter, long coord"_test = []{
-    // Parametro invalido con stringa piu' lunga
-    // di due caratteri
-    chess::Coords c = chess::Coords("f10");
-
-    expect(c.file == 255_u8);
-    expect(c.rank == 255_u8);
-  };
-
-  "Empty constructor"_test = []{
-    chess::Coords c = chess::Coords();
-
-    expect(c.file == 255_u8);
-    expect(c.rank == 255_u8);
-  };
-
-
-  "String constructor a1"_test = []{
-    
-    chess::Coords c = chess::Coords("a1");
-
-    expect(c.file == 0_i);
-    expect(c.rank == 0_i);
-  };
-
-  "String constructor a8"_test = []{
-    
-    chess::Coords c = chess::Coords("a8");
-
-    expect(c.file == 7_i);
-    expect(c.rank == 0_i);
-  };
-
-  "String constructor h8"_test = []{
-    
-    chess::Coords c = chess::Coords("h8");
-
-    expect(c.file == 7_i);
-    expect(c.rank == 7_i);
-  };
-
-  "String constructor h1"_test = []{
-    
-    chess::Coords c = chess::Coords("h1");
-
-    expect(c.file == 0_i);
-    expect(c.rank == 7_i);
-  };
-  
   "Equal operator"_test = []{
     
     chess::Coords c1 = chess::Coords("h1");
