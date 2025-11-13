f52135f70c9adc4d0b362c3c1b43cb43ca65d23f
Thomas <thomasghionesanremo@gmail.com>
2025-11-13 12:15:10 +0100

    refactor piece.hpp

--- piece/piece.hpp
+++ piece/piece.hpp
@@ -8,27 +8,6 @@ namespace pieces {
 
 using U64 = uint64_t;
 
-/*
-enum Type {
-    EMPTY,  // 0000
-    PAWN,   // 0001
-    KNIGHT, // 0010
-    BISHOP, // 0011
-    ROOK,   // 0100
-    QUEEN,  // 0101
-    KING    // 0110
-}; */
-
-// ==================== INIT / BITBOARD BUILDERS ====================
-U64 getPieceBitboard(uint8_t piece, const U64& boardBitboard) noexcept;
-
-U64 initPawnBitboard(int16_t squareIndex) noexcept;
-U64 initKnightBitboard(int16_t squareIndex) noexcept;
-U64 initBishopBitboard(int16_t squareIndex) noexcept;
-U64 initRookBitboard(int16_t squareIndex) noexcept;
-U64 initQueenBitboard(int16_t squareIndex) noexcept;
-U64 initKingBitboard(int16_t squareIndex) noexcept;
-
 // ==================== ATTACK MAPS (color-agnostic salvo pedone) ====================
 U64 getPawnAttacks(int16_t squareIndex, bool isWhite) noexcept;   // solo catture diagonali
 U64 getPawnForwardPushes(int16_t squareIndex, bool isWhite, U64 occupancy) noexcept; // avanzamenti (1 o 2 caselle se libere)
