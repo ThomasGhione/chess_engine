f8df976776b91a395b6c158cac64c3de09d22969
Thomas <thomasghionesanremo@gmail.com>
2025-11-18 09:09:37 +0100

    fixed knight/king tests

--- piece/test/testKnight.cpp
+++ piece/test/testKnight.cpp
@@ -1,12 +1,14 @@
 #include "../../tests/ut.hpp"
 #include "../../piece/piece.hpp"
+#include "../../coords/coords.hpp"
 
 #include <iostream>
 
 namespace ut = boost::ut;
 
 uint64_t coordToIndex(std::string coord){
-  return ( '8' - coord.at(1)) *8 + ('h' - coord.at(0));
+  chess::Coords c(coord);
+  return c.toIndex();
 }
 
 ut::suite knightSuite = [] {
@@ -40,8 +42,8 @@ ut::suite knightSuite = [] {
     uint64_t output = pieces::getKnightAttacks(coordToIndex("a1")); 
     
     uint64_t expect_output = 0;
-    expect_output |= (1 << coordToIndex("b3"));
-    expect_output |= (1 << coordToIndex("c2"));
+    expect_output |= (1ULL << coordToIndex("b3"));
+    expect_output |= (1ULL << coordToIndex("c2"));
     
     expect(expect_output == output);
   };
@@ -50,8 +52,8 @@ ut::suite knightSuite = [] {
     uint64_t output = pieces::getKnightAttacks(coordToIndex("h1")); 
     
     uint64_t expect_output = 0;
-    expect_output |= (1 << coordToIndex("g3"));
-    expect_output |= (1 << coordToIndex("f2"));
+    expect_output |= (1ULL << coordToIndex("g3"));
+    expect_output |= (1ULL << coordToIndex("f2"));
     
     expect(expect_output == output);
   };
@@ -60,8 +62,8 @@ ut::suite knightSuite = [] {
     uint64_t output = pieces::getKnightAttacks(coordToIndex("a8")); 
     
     uint64_t expect_output = 0;
-    expect_output |= (1 << coordToIndex("b6"));
-    expect_output |= (1 << coordToIndex("c7"));
+    expect_output |= (1ULL << coordToIndex("b6"));
+    expect_output |= (1ULL << coordToIndex("c7"));
     
     expect(expect_output == output);
   };
@@ -70,8 +72,36 @@ ut::suite knightSuite = [] {
     uint64_t output = pieces::getKnightAttacks(coordToIndex("h8")); 
     
     uint64_t expect_output = 0;
-    expect_output |= (1 << coordToIndex("g6"));
-    expect_output |= (1 << coordToIndex("f7"));
+    expect_output |= (1ULL << coordToIndex("g6"));
+    expect_output |= (1ULL << coordToIndex("f7"));
+    
+    expect(expect_output == output);
+  };
+
+  "getKnightAttacks edge a4"_test =[]{
+    uint64_t output = pieces::getKnightAttacks(coordToIndex("a4")); 
+    
+    uint64_t expect_output = 0;
+    expect_output |= (1ULL << coordToIndex("b6"));
+    expect_output |= (1ULL << coordToIndex("c5"));
+    expect_output |= (1ULL << coordToIndex("c3"));
+    expect_output |= (1ULL << coordToIndex("b2"));
+    
+    expect(expect_output == output);
+  };
+
+  "getKnightAttacks center e4"_test =[]{
+    uint64_t output = pieces::getKnightAttacks(coordToIndex("e4")); 
+    
+    uint64_t expect_output = 0;
+    expect_output |= (1ULL << coordToIndex("d6"));
+    expect_output |= (1ULL << coordToIndex("f6"));
+    expect_output |= (1ULL << coordToIndex("c5"));
+    expect_output |= (1ULL << coordToIndex("g5"));
+    expect_output |= (1ULL << coordToIndex("c3"));
+    expect_output |= (1ULL << coordToIndex("g3"));
+    expect_output |= (1ULL << coordToIndex("d2"));
+    expect_output |= (1ULL << coordToIndex("f2"));
     
     expect(expect_output == output);
   };
