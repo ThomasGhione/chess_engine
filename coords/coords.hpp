c74b0e2e8f1f7ebfdbfa92e8e1c368d8550be899
Thomas <thomasghionesanremo@gmail.com>
2025-12-04 12:46:47 +0100

    fix: slightly faster coords

--- coords/coords.hpp
+++ coords/coords.hpp
@@ -19,51 +19,51 @@ public:
 
     // constructors
 
-    Coords() noexcept = default;
+    constexpr Coords() noexcept = default;
     
-    Coords(uint8_t index) noexcept : Coords() {
+    constexpr Coords(uint8_t index) noexcept : Coords() {
         if (index < 64) {
-            this->file = index & 7;
-            this->rank = index >> 3;
+            file = index & 7;
+            rank = index >> 3;
         }
     }
 
-    Coords(uint8_t f, uint8_t r) noexcept : Coords() {
-        if (this->isValid(f)) this->file = f;
-        if (this->isValid(r)) this->rank = r;
+    constexpr Coords(uint8_t f, uint8_t r) noexcept : Coords() {
+        if (isValid(f)) file = f;
+        if (isValid(r)) rank = r;
     }
 
     Coords(const std::string& input) noexcept : Coords() {
         if (input.length() != 2) return;
-        if (this->isLetter(std::tolower(input[0]))) file = std::tolower(input[0]) - 'a';
-        if (this->isNumber(input[1])) rank = input[1] - '1';
+        char f = std::tolower(input[0]);
+        if (isLetter(f)) file = f - 'a';
+        if (isNumber(input[1])) rank = input[1] - '1';
     }
 
-    Coords(const Coords& c) noexcept = default;
+    constexpr Coords(const Coords& c) noexcept = default;
 
 
 
     // operator overloads
 
-    bool operator==(const Coords &other) const noexcept { return (this->file == other.file) && (this->rank == other.rank);}
-    bool operator!=(const Coords &other) const noexcept { return !(*this == other);}
+    constexpr bool operator==(const Coords &other) const noexcept { return (file == other.file) && (rank == other.rank);}
+    constexpr bool operator!=(const Coords &other) const noexcept { return !(*this == other);}
     
-    Coords& operator=(const Coords &other) noexcept {
-        if (this != &other) {
-            this->file = other.file;
-            this->rank = other.rank;
-        } return *this;
+    constexpr Coords& operator=(const Coords &other) noexcept {
+        file = other.file;
+        rank = other.rank;
+        return *this;
     }
 
 
     // setters / updaters
 
-    bool update(const Coords& other) noexcept{ return this->update(other.file, other.rank); } 
+    constexpr bool update(const Coords& other) noexcept{ return update(other.file, other.rank); } 
     
-    bool update(const uint8_t f, const uint8_t r) noexcept{
-        if (f == INVALID_COORDS || r == INVALID_COORDS) return false;
-        this->file = f;
-        this->rank = r;
+    constexpr bool update(const uint8_t f, const uint8_t r) noexcept{
+        if (!isValid(f) || !isValid(r)) return false;
+        file = f;
+        rank = r;
         return true;
     }
 
@@ -71,28 +71,28 @@ public:
     
     // utility static methods
 
-    static bool isValid(uint8_t x) noexcept { return (x < 8); }
-    static bool isLetter(char c) noexcept { return ((c >= 'a' && c <= 'h') || (c >= 'A' && c <= 'H')); }
-    static bool isNumber(char c) noexcept { return (c >= '1') && (c <= '8'); }
-    static bool isInBounds(const Coords& coords) noexcept { return (isValid(coords.file) && isValid(coords.rank)); }
+    static constexpr bool isValid(uint8_t x) noexcept { return (x < 8); }
+    static constexpr bool isLetter(char c) noexcept { return ((c >= 'a' && c <= 'h') || (c >= 'A' && c <= 'H')); }
+    static constexpr bool isNumber(char c) noexcept { return (c >= '1') && (c <= '8'); }
+    static constexpr bool isInBounds(const Coords& coords) noexcept { return (isValid(coords.file) && isValid(coords.rank)); }
     
     
     
 
     // conversion methods
 
-    std::string toString() noexcept {
+    std::string toString() const noexcept {
         std::string result(2, ' ');
         if (isInBounds(*this)) {
-            result[0] = static_cast<char>('a' + this->file);
-            result[1] = static_cast<char>('1' + this->rank);
+            result[0] = static_cast<char>('a' + file);
+            result[1] = static_cast<char>('1' + rank);
         } else {
             result = "??";
         }
         return result;
     }
     
-    uint8_t toIndex() const noexcept { return static_cast<uint8_t>(this->rank * 8 + this->file); }
+    constexpr uint8_t toIndex() const noexcept { return static_cast<uint8_t>(rank * 8 + file); }
     
     static std::string toAlgebric(const Coords& c) noexcept {
         char fileChar = static_cast<char>('a' + c.file);
