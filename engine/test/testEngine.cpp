5d90e940d6277a247227cb84314af3fa2fcdd9f1
Daniele <daniele.ferretti.uni@gmail.com>
2025-12-16 18:50:57 +0100

    test about mate in one or two moves

--- engine/test/testEngine.cpp
+++ engine/test/testEngine.cpp
@@ -8,71 +8,6 @@ namespace ut = boost::ut;
 ut::suite engineSuite = [] {
   using namespace ut;
 
-  "Default constructor"_test = []{
-    engine::Engine e = engine::Engine();
-    
-    // Attesa una board vuota
-  };
-
-  "playGameVsHuman"_test = []{
-    engine::Engine e = engine::Engine();
-
-    // Scrivere mosse per matto dello scolaro:
-    // e4-e5 Bc4-Kc6 Qf3-a6 Qxf3#
-    // Attesa vittoria del bianco e interruzione gioco
-  };
-  
-  "playGameVsHuman"_test = []{
-    engine::Engine e = engine::Engine();
-
-    // Scrivere mosse per matto dell'imbecille:
-    // f4-e6 g4-Qh4#
-    // Attesa vittoria del nero e interruzione gioco
-  };
-
-  "isMate"_test = []{
-    // Creare board con fen:
-    // k6R/6R1/8/8/8/8/8/2K5 w - - 0 1
-    // Attesa isMate: true
-  };
-  
-  "isMate"_test = []{
-    // Creare board con fen:
-    // k7/6R1/7R/8/8/8/8/2K5 w - - 0 1
-    // Attesa isMate: false
-  };
-
-
-  "isMate"_test = []{
-    // Creare board con fen:
-    // rn1qkb1r/pp1bpppp/2pN1n2/8/8/5N2/PPPPQPPP/R1B1KB1R b KQkq - 4 6
-    // Attesa isMate: true
-  };
-
-  "isMate"_test = []{
-    // Creare board con fen:
-    // 1k6/8/8/8/3rrr2/8/8/R3K2R w KQ - 0 1
-    // Attesa isMate: false
-  };
-
-  "isMate"_test = []{
-    // Creare board con fen:
-    // k7/8/8/8/5n2/5nn1/8/7K w - - 0 1
-    // Attesa isMate: true
-  };
-
-  "isMate"_test = []{
-    // Creare board con fen:
-    // r3k2r/pp1nQppp/5n2/3p4/1B6/P4N2/1Pq2PPP/RN2K2R b KQkq - 0 12
-    // Attesa isMate: true
-  };
-  
-  "isMate"_test = []{
-    // Creare board con fen:
-    // r1bk2nr/pppp1ppp/2n5/1N6/4q3/2P5/P1P1BPPP/R1BQ1RK1 b - - 2 9
-    // Attesa isMate: false
-  };
-
   /*
   "getMaterialDelta FAST vs NORMAL vs SLOW"_test = []{
     engine::Engine e = engine::Engine();
@@ -100,11 +35,8 @@ ut::suite engineSuite = [] {
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count();
     printf("Normal material delta calculated in: %lu\n", duration2);
 
-    
     expect(duration1 < duration2);
   };
-*/
-
 
   "performance searchPosition depth 6"_test = []{
     engine::Engine e = engine::Engine();
@@ -120,6 +52,7 @@ ut::suite engineSuite = [] {
     printf("Nodes searched: %lu\n", engine::Engine::nodesSearched);
     expect(duration < 200);
   };
+  */
 
   /*
   "performance searchPosition depth 6"_test = []{
@@ -138,6 +71,7 @@ ut::suite engineSuite = [] {
   };
   */
 
+  /*
   "avg performance searchPosition depth 6 over 20 runs"_test = []{
     engine::Engine e = engine::Engine();
     e.depth = 6;
@@ -161,8 +95,7 @@ ut::suite engineSuite = [] {
     printf("Average Depth 6 search time over %d runs: %.2f ms\n", runs, avgDuration);
     expect(avgDuration < 500);
   };
-  
-  /*
+
   "calculate 1mln nodes"_test = []{
     engine::Engine e = engine::Engine();
     e.depth = 6;
@@ -181,7 +114,7 @@ ut::suite engineSuite = [] {
     printf("1 million nodes searched in %lu ms\n", duration);
     expect(duration < 5000);
   };
-  */
+  
 
   "is engine generating & sorting castling rights correctly"_test = []{
     engine::Engine e = engine::Engine("8/6pp/6p1/6pk/2p1p1p1/1pPpPpPp/1P1P1P1P/3NK2R w K - 0 1");
@@ -266,5 +199,5 @@ ut::suite engineSuite = [] {
     expect(moves.size == 2) // Expected number of legal moves
       << "Expected 2 legal moves, got " << moves.size << '\n';
   };
-  
+  */
 };
