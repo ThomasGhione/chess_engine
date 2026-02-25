b15619757e3b8c03cf3b2b56b4f33eaba908ce33
Thomas <thomasghionesanremo@gmail.com>
2026-02-25 08:10:14 +0100

    FIX: update cloc exclusions to include 'saves' directory and 'make' language

--- script/count_loc.sh
+++ script/count_loc.sh
@@ -2,7 +2,7 @@
 # Count lines of code for the chess engine including tests (excluding only stockfish, doc, script, games, and ut.hpp library)
 
 cloc . \
-  --exclude-dir=stockfish,doc,script,games \
-  --exclude-lang=JSON,Markdown \
+  --exclude-dir=output,doc,script,games,saves \
+  --exclude-lang=JSON,Markdown,make \
   --not-match-f='ut\.hpp' \
   --by-file-by-lang
