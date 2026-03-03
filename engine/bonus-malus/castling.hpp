530969f6a2fd9287f515029d6ac4e3c8f8bdc5ed
Thomas <thomasghionesanremo@gmail.com>
2026-03-03 11:41:53 +0100

    FEAT: adjust castling penalty values and refine king hook pawn hanging penalty for improved evaluation

--- engine/bonus-malus/castling.hpp
+++ engine/bonus-malus/castling.hpp
@@ -12,7 +12,7 @@ inline static constexpr int64_t CASTLING_BONUS = 35;
 
 inline static constexpr int64_t KING_NON_CASTLING_PENALTY = 10;
 inline static constexpr int64_t KING_LOST_CASTLING_RIGHTS_PENALTY = 50;
-inline static constexpr int64_t LOSS_OF_CASTLING_PENALTY = 120;
+inline static constexpr int64_t LOSS_OF_CASTLING_PENALTY = 40;
 
 } // namespace engine
 
--- engine/bonus-malus/king_safety_activity.hpp
+++ engine/bonus-malus/king_safety_activity.hpp
@@ -21,7 +21,7 @@ inline static constexpr int64_t KING_CASTLED_SHIELD_BREAK_PENALTY = 10;
 inline static constexpr int64_t KING_SHELTER_ADVANCE_ONE_PENALTY = 4;
 inline static constexpr int64_t KING_SHELTER_ADVANCE_TWO_PENALTY = 9;
 inline static constexpr int64_t KING_HOOK_PAWN_ATTACKED_PENALTY = 40;
-inline static constexpr int64_t KING_HOOK_PAWN_HANGING_PENALTY = 100;
+inline static constexpr int64_t KING_HOOK_PAWN_HANGING_PENALTY = 45;
 inline static constexpr int64_t KING_SAFETY_OPENING_SCALE_PERCENT = 55;
 
 // Open files/diagonals toward king.
