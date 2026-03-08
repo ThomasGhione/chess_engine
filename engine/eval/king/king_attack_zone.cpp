272d5918d9e06bbd522760fe5d33b14e05b3c5e1
DanieleFerretti03 <daniele.ferretti.uni@gmail.com>
2026-03-08 14:54:29 +0100

    Refactor NOT tested

---
+++ engine/eval/king/king_attack_zone.cpp
@@ -0,0 +1,80 @@
+#include "../evaluator.hpp"
+
+namespace engine {
+
+inline void Evaluator::accumulateKingZoneAttackersAll(const chess::Board& b, int side, uint64_t kingZone, uint64_t occ,
+                                                      uint64_t developedKnights, uint64_t developedBishops,
+                                                      int& attackerCount, int32_t& attackWeight) noexcept {
+    accumulateKingZoneAttackers<knightAttacksLookup, engine::KING_ATTACK_WEIGHT_KNIGHT>(
+        developedKnights, kingZone, occ, attackerCount, attackWeight);
+    accumulateKingZoneAttackers<pieces::getBishopAttacks, engine::KING_ATTACK_WEIGHT_BISHOP>(
+        developedBishops, kingZone, occ, attackerCount, attackWeight);
+    accumulateKingZoneAttackers<pieces::getRookAttacks, engine::KING_ATTACK_WEIGHT_ROOK>(
+        b.rooks_bb[side], kingZone, occ, attackerCount, attackWeight);
+    accumulateKingZoneAttackers<pieces::getQueenAttacks, engine::KING_ATTACK_WEIGHT_QUEEN>(
+        b.queens_bb[side], kingZone, occ, attackerCount, attackWeight);
+}
+
+inline int32_t Evaluator::evalKingAttackZoneSide(const chess::Board& b, const AttackData data[2], int side, uint64_t occ) noexcept {
+    static constexpr int ATTACKER_SCALE_PERCENT[9] = {0, 0, 32, 52, 68, 80, 90, 97, 100};
+    constexpr uint64_t WHITE_MINOR_START = 0xFF00000000000000ULL;
+    constexpr uint64_t BLACK_MINOR_START = 0x000000000000FFFFULL;
+
+    const int oppSide = side ^ 1;
+    const int sign = (side == 0) ? 1 : -1;
+
+    const uint64_t enemyKingBB = b.kings_bb[oppSide];
+    if (!enemyKingBB) [[unlikely]] return 0;
+
+    const int enemyKingSq = __builtin_ctzll(enemyKingBB);
+    const uint64_t kingZone = pieces::KING_ATTACKS[enemyKingSq] | chess::Board::bitMask(enemyKingSq);
+
+    const uint64_t developedKnights = (side == 0)
+        ? (b.knights_bb[side] & ~WHITE_MINOR_START)
+        : (b.knights_bb[side] & ~BLACK_MINOR_START);
+    const uint64_t developedBishops = (side == 0)
+        ? (b.bishops_bb[side] & ~WHITE_MINOR_START)
+        : (b.bishops_bb[side] & ~BLACK_MINOR_START);
+
+    int attackerCount = 0;
+    int32_t attackWeight = 0;
+
+    accumulateKingZoneAttackersAll(b, side, kingZone, occ, developedKnights, developedBishops, attackerCount, attackWeight);
+    if (attackerCount < 2) return 0;
+
+    const uint64_t defenderMap = data[oppSide].allAttacks | pieces::KING_ATTACKS[enemyKingSq];
+    const uint64_t zoneAttacks = data[side].allAttacks & kingZone;
+    const int safeContacts = __builtin_popcountll(zoneAttacks & ~defenderMap);
+    const int forcingContacts = __builtin_popcountll(zoneAttacks & defenderMap);
+
+    int32_t attackUnits = attackWeight
+        + safeContacts * engine::KING_SAFE_CONTACT_BONUS
+        + forcingContacts * engine::KING_FORCING_CONTACT_BONUS;
+
+    addAllKingCheckUnits(b, side, enemyKingSq, defenderMap, occ, attackUnits);
+
+    const int scaleIndex = std::min(attackerCount, 8);
+    int32_t attackDanger = (attackUnits * ATTACKER_SCALE_PERCENT[scaleIndex]) / 100;
+    attackDanger = std::min<int32_t>(attackDanger, engine::KING_ATTACK_DANGER_CAP);
+
+    return sign * attackDanger;
+}
+
+int32_t Evaluator::evalKingAttackZone(const chess::Board& b, const AttackData data[2]) noexcept {
+    const uint64_t occ = b.getPiecesBitMap();
+    return evalKingAttackZoneSide(b, data, 0, occ) + evalKingAttackZoneSide(b, data, 1, occ);
+}
+
+inline void Evaluator::addAllKingCheckUnits(const chess::Board& b, int side, int enemyKingSq, uint64_t defenderMap, uint64_t occ, int32_t& attackUnits) noexcept {
+    const uint64_t knightChecks = b.knights_bb[side] & pieces::KNIGHT_ATTACKS[enemyKingSq];
+    const uint64_t bishopChecks = b.bishops_bb[side] & pieces::getBishopAttacks(enemyKingSq, occ);
+    const uint64_t rookChecks = b.rooks_bb[side] & pieces::getRookAttacks(enemyKingSq, occ);
+    const uint64_t queenChecks = b.queens_bb[side] & pieces::getQueenAttacks(enemyKingSq, occ);
+
+    addKingCheckUnits(knightChecks, defenderMap, engine::KING_SAFE_CHECK_BONUS, engine::KING_FORCING_CHECK_BONUS, attackUnits);
+    addKingCheckUnits(bishopChecks, defenderMap, engine::KING_SAFE_CHECK_BONUS, engine::KING_FORCING_CHECK_BONUS, attackUnits);
+    addKingCheckUnits(rookChecks, defenderMap, engine::KING_SAFE_CHECK_BONUS + 4, engine::KING_FORCING_CHECK_BONUS + 2, attackUnits);
+    addKingCheckUnits(queenChecks, defenderMap, engine::KING_SAFE_CHECK_BONUS + 8, engine::KING_FORCING_CHECK_BONUS + 4, attackUnits);
+}
+
+} // namespace engine
