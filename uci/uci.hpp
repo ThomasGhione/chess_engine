fd46d0c25c64cafe633931217dd7aa80ef5a8b49
Thomas <thomasghionesanremo@gmail.com>
2026-04-19 20:21:50 +0200

    REFACTOR: rename xorPiecesFromBitboard to xorPiecesFromBB for consistency

--- uci/uci.hpp
+++ uci/uci.hpp
@@ -9,36 +9,36 @@ namespace engine {
 
 namespace uci {
 
-    class UCI {
+class UCI {
 
-        public:
-            UCI();
-            UCI(engine::Engine& engine);
+public:
 
-            engine::Engine& engine;
+    UCI();
+    UCI(engine::Engine& engine);
 
+    engine::Engine& engine;
 
-            [[noreturn]] void mainLoop() noexcept;
+    [[noreturn]] void mainLoop() noexcept;
 
-            // Parser
-            void parseCommand(std::string_view command) noexcept;
+    // Parser
+    void parseCommand(std::string_view command) noexcept;
 
-            // Standard UCI commands
-            void quit() noexcept;
-            void uci() noexcept;
-            void setOption(std::string_view args) noexcept;
-            void position(std::string_view command) noexcept;
-            void ucinewgame() noexcept;
-            void isready() noexcept;
-            void go(std::string_view args) noexcept;
-            void stop() noexcept;
-            void ponderhit() noexcept;
+    // Standard UCI commands
+    void quit() noexcept;
+    void uci() noexcept;
+    void setOption(std::string_view args) noexcept;
+    void position(std::string_view command) noexcept;
+    void ucinewgame() noexcept;
+    void isready() noexcept;
+    void go(std::string_view args) noexcept;
+    void stop() noexcept;
+    void ponderhit() noexcept;
 
-        private:
+private:
 
-            void parseMoves(std::string_view moves) noexcept;
-            void parseFEN(std::string_view fen) noexcept;
+    void parseMoves(std::string_view moves) noexcept;
+    void parseFEN(std::string_view fen) noexcept;
 
-    }; // class UCI
+}; // class UCI
 
 }
