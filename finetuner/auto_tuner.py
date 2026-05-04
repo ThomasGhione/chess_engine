b05258de50a8c67f54d475a837df29b0d8a31aa0
Thomas <thomasghionesanremo@gmail.com>
2026-05-04 08:22:24 +0200

    FEAT: auto_tuner list now takes pair of (weight_name, weight_value)

--- finetuner/auto_tuner.py
+++ finetuner/auto_tuner.py
@@ -10,21 +10,29 @@ list_of_constants = []
 
 def build_list_of_constants_from_hpp(file):
     line = file.find('int32_t')
-    start_idx = file.find('=', line)
-    end_idx = file.find(';', start_idx)
+    name_idx = file.find('int32_t', line)
+    value_idx = file.find('=', line)
+    end_idx = file.find(';', value_idx)
 
-    while start_idx != -1 and end_idx != -1:
-        eval_data = file[start_idx + 1:end_idx].strip()
+    while value_idx != -1 and end_idx != -1:
+        
+        w_name = file[name_idx + len('int32_t') : value_idx - 1].strip()
+        w_value = file[value_idx + len('=') : end_idx].strip()
 
-        if eval_data.find("'") != -1:
-            eval_data = eval_data.replace("'", "")
+        # Remove any single quotes from number literals (e.g., 1'000 -> 1000)
+        if w_value.find("'") != -1:
+            w_value = w_value.replace("'", "")
 
-        if eval_data.find("{") == -1 and eval_data.find("max") == -1:        
-            list_of_constants.append(eval_data)
+
+        if w_value.find("{") == -1 and w_value.find("max") == -1:
+            pair = (w_name, w_value)        
+            list_of_constants.append(pair)
+            
 
         line = file.find('int32_t', end_idx)
-        start_idx = file.find('=', line)
-        end_idx = file.find(';', start_idx)
+        name_idx = file.find('int32_t', line)
+        value_idx = file.find('=', line)
+        end_idx = file.find(';', value_idx)
 
 def main():
     weights_data = pd.read_csv(csv_file)
