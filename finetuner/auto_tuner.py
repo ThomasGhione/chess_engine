62b5724d47813e8d78a411bdf4c165d6da3f209e
Thomas <thomasghionesanremo@gmail.com>
2026-05-04 09:58:00 +0200

    FEAT: added fen parsing in auto tuner

--- finetuner/auto_tuner.py
+++ finetuner/auto_tuner.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 eval_constants_file = ROOT_DIR / 'engine' / 'eval_constants.hpp'
-csv_file = ROOT_DIR / 'finetuner' / 'auto_tuner.csv'
+auto_tuner_csv = ROOT_DIR / 'finetuner' / 'auto_tuner.csv'
+fen_csv = ROOT_DIR / 'finetuner' / 'fen_data.csv'
 
 list_of_weights = []
+list_of_fens = []
 
-def build_list_of_weights_from_hpp(file):
+def parse_weights_from_header(file):
     line = file.find('int32_t')
     name_idx = file.find('int32_t', line)
     value_idx = file.find('=', line)
@@ -37,20 +39,33 @@ def build_list_of_weights_from_hpp(file):
         value_idx = file.find('=', line)
         end_idx = file.find(';', value_idx)
 
+
 def write_weights_to_csv():
     data = pd.DataFrame(list_of_weights, 
                         columns=['w_name', 'w_default', 'w_min', 'w_max', 'step', 'enabled', 'group'])
-    data.to_csv(csv_file, index=False)
+    data.to_csv(auto_tuner_csv, index=False)
+
+
+def parse_fen_from_csv():
+    data = pd.read_csv(fen_csv)
+    for row in data.itertuples():
+        list_of_fens.append((row.fen))
+
 
 
 def main():
-    weights_data = pd.read_csv(csv_file)
+    # weights_data = pd.read_csv(auto_tuner_csv)
 
     with eval_constants_file.open('r', encoding='utf-8') as eval_constants:
-        build_list_of_weights_from_hpp(eval_constants.read())
+        parse_weights_from_header(eval_constants.read())
 
     write_weights_to_csv()
 
+
+
+    parse_fen_from_csv()
+
+    
     # Debug only!
     print(*list_of_weights, sep = '\n')
 
